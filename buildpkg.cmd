29c0884850b5574e690327d837513d462301ed1d
John Jamieson <jjamieson@eit.ac.nz>
2023-07-17 12:47:20 +1200

    updated buld files for newer go builds

--- buildpkg.cmd
+++ buildpkg.cmd
@@ -1,6 +1,7 @@
 @echo off
 set GO111MODULE=on
 set GOFLAGS=-mod=mod
+set GOWORK=off
 go mod download
 :: strip debug info during build
 go build -ldflags="-s -w" .
--- buildvendor.cmd
+++ buildvendor.cmd
@@ -1,6 +1,7 @@
 @echo off
 set GO111MODULE=on
 set GOFLAGS=-mod=vendor
+set GOWORK=off
 go mod vendor
 :: strip debug info during build
 go build -ldflags="-s -w" .
